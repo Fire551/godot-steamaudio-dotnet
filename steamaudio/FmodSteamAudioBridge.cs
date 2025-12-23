175ff7a1ed3df79eb23cb5592a77e213035bcab0
John-Daniel Doody <JamDoggie3@gmail.com>
2025-12-23 01:10:59 -0500

    move scene commit back to simulation thread

--- steamaudio/FmodSteamAudioBridge.cs
+++ steamaudio/FmodSteamAudioBridge.cs
@@ -858,16 +858,6 @@ namespace SteamAudioDotnet.scripts.nativelib
             if (Engine.IsEditorHint())
                 return;
 
-            if (SceneCommitQueued)
-            {
-                lock (SteamAudioSimulationLock)
-                {
-                    API.iplSceneCommit(Scene);
-                    API.iplSimulatorCommit(Simulator);
-                    SceneCommitQueued = false;
-                }
-            }
-
             if (ListenerNode == null
                 || Simulator == IntPtr.Zero)
             {
@@ -925,6 +915,16 @@ namespace SteamAudioDotnet.scripts.nativelib
 
                 if (Context == IntPtr.Zero || Simulator == IntPtr.Zero)
                     continue;
+
+                if (SceneCommitQueued)
+                {
+                    lock (SteamAudioSimulationLock)
+                    {
+                        API.iplSceneCommit(Scene);
+                        API.iplSimulatorCommit(Simulator);
+                        SceneCommitQueued = false;
+                    }
+                }
 
                 // Run direct
                 API.iplSimulatorSetSharedInputs(Simulator, SimulationFlags.Direct, ref SharedInputs);
